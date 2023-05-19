e32dfe5c61630266637664107f85248eef4cf584
DodoExtinguiendoTusProblemas <130880697+DodoExtinguiendoTusProblemas@users.noreply.github.com>
2023-05-19 15:25:13 -0400

    Actualización en Envirionment
    se actualizó la base de datos del proyecto

--- proyecto DuocUCMaps/src/environments/environment.prod.ts
+++ proyecto DuocUCMaps/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig :{
-    apiKey: "AIzaSyBpkkKdZlTRPiF6-UpsJvM6eIvFDEDJUuY",
-    authDomain: "uwuntu-da6f6.firebaseapp.com",
-    projectId: "uwuntu-da6f6",
-    storageBucket: "uwuntu-da6f6.appspot.com",
-    messagingSenderId: "1094060813292",
-    appId: "1:1094060813292:web:aa77c3a91ca068f9f7eec9",
-    measurementId: "G-SLT458NTPR"
+    apiKey: "AIzaSyAH4komYvtvuAI5eUZuxjrL-ntpRd6P9xU",
+    authDomain: "duocmaps-e980c.firebaseapp.com",
+    projectId: "duocmaps-e980c",
+    storageBucket: "duocmaps-e980c.appspot.com",
+    messagingSenderId: "207293455180",
+    appId: "1:207293455180:web:21a14bdd5fb696dfe1cde9",
+    measurementId: "G-E85EEXGDFN"
   }
 };
